9599f8faa62f402722c406093a0cde1da0f81285
Ricardo Chavarria <ricardo.chavarria@cit.hn>
2020-04-28 21:37:56 -0600

    Introduccion a Webpakc

---
+++ src/components/component.js
@@ -0,0 +1,5 @@
+import './component.css';
+
+const element = document.createElement('h1');
+element.innerHTML = "Webpack";
+document.body.append(element);
---
+++ webpack.prod.js
@@ -0,0 +1,61 @@
+
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const {
+    CleanWebpackPlugin
+} = require('clean-webpack-plugin');
+
+module.exports = {
+    mode:'production',
+    entry: path.resolve(__dirname, 'src/index.js'),
+    output:{
+        filename: 'main.js',
+        path: path.resolve(__dirname, 'dist')
+    },
+    module:{
+        rules:[
+            {
+                test: /\.(png|jpg|svg|gif|mp4)$/,
+                use:[
+                    {
+                        loader:'file-loader',
+                        options:{
+                            esModule: false,
+                            name: 'assets/images/[name].[ext]'
+                        }
+                    }
+                ]
+            },
+            {
+             test:/\.css$/,
+             exclude: /styles\.css$/,
+             use:['style-loader', 'css-loader']
+            },
+            {
+                test: /styles\.css$/,
+                use:[
+                    {
+                        loader: MiniCssExtractPlugin.loader
+                    },
+                    'css-loader'
+                ]
+            },
+            {
+                test: /\.html$/,
+                loader: 'html-loader'
+            }
+        ]
+    },
+    plugins:[
+        new HtmlWebpackPlugin({
+            template: './src/index.html',
+            filename: 'index.html'
+        }),
+        new MiniCssExtractPlugin({
+            filename: '[name].css',
+            ignoreOrder: false
+        }),
+        //new CleanWebpackPlugin()
+    ]
+}
